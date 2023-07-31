d3b74007aebc750a0982e1b6148e6b60de40ade3
GoodGod <guskov.workakk@gmail.com>
2023-07-31 18:15:01 +0500

    Init commit

---
+++ WorkLibary/Program.cs
@@ -0,0 +1,235 @@
+﻿using OfficeOpenXml;
+
+var fi = new FileInfo(@"A.xlsx");
+var users = new List<User>();
+using (var p = new ExcelPackage(fi))
+{
+    var ws = p.Workbook.Worksheets["Sheet"];
+    var rows = 42;
+    for (var row = 2; row < rows; row++)
+    {
+        var user = new User((string) ws.Cells[row, 3].Value);
+        user.AddOrder(ws.Cells[row, 5].Value, ws.Cells[row, 6].Value, ws.Cells[row, 7].Value, ws.Cells[row, 8].Value,
+            ws.Cells[row, 9].Value, ws.Cells[row, 10].Value);
+
+        users.Add(user);
+    }
+    
+    p.Save();
+}
+
+using (var p = new ExcelPackage())
+{
+    var column = 1;
+    var row = 1;
+    
+    foreach (var day in new[] {Days.Tuesday, Days.Wednesday, Days.Thursday, Days.Friday})
+    {
+        var ws = p.Workbook.Worksheets.Add(day.ToString());
+        foreach (var user in users)
+        {
+            ws.Cells[row, column++].Value = user.Name;
+            ws.Cells[row, column++].Value = user.Orders[(int) day].OrderTime;
+            ws.Cells[row, column++].Value = user.Orders[(int) day].Lunch;
+            ws.Cells[row, column++].Value = user.Orders[(int) day].HotFood;
+            ws.Cells[row, column++].Value = user.Orders[(int) day].Soup;
+            ws.Cells[row, column++].Value = user.Orders[(int) day].Bakery;
+            ws.Cells[row, column++].Value = user.Orders[(int) day].WillCoffee;
+            
+            // ws.Cells[row++, column].Value = user.Name;
+            // ws.Cells[row++, column].Value = user.Orders[(int) day].OrderTime;
+            // ws.Cells[row++, column].Value = user.Orders[(int) day].Lunch;
+            // ws.Cells[row++, column].Value = user.Orders[(int) day].HotFood;
+            // ws.Cells[row++, column].Value = user.Orders[(int) day].Soup;
+            // ws.Cells[row++, column].Value = user.Orders[(int) day].Bakery;
+            // ws.Cells[row++, column].Value = user.Orders[(int) day].WillCoffee;
+        }
+
+        row++;
+        column = 1;
+    }
+    
+    p.SaveAs(new FileInfo(@"AAA.xlsx"));
+}
+
+Console.WriteLine();
+
+public enum Days
+{
+    Tuesday = 0,
+    Wednesday = 1,
+    Thursday = 2,
+    Friday = 3
+}
+
+public enum OrderTime
+{
+    Morning,
+    Day,
+    Night,
+    Default
+}
+
+public enum Lunch
+{
+    Manager1,
+    Manager2, 
+    BusinessLady1, 
+    BusinessLady2, 
+    Freelancer1, 
+    Freelancer2, 
+    Gamer1, 
+    Gamer2, 
+    Vegan1, 
+    Vegan2, 
+    Vegan3, 
+    Prince1, 
+    Prince2, 
+}
+
+public enum Soup
+{
+    SpinachSoup, 
+    MushroomSoup, 
+    PumpkinSoup, 
+}
+
+public enum HotFood
+{
+    Pork, 
+    Beef, 
+    Chicken, 
+    Shrimp, 
+    FalafelBeans, 
+    FalafelChickpea, 
+    FalafelBuckwheat, 
+    KebabChicken, 
+    KebabPork, 
+}
+
+public enum Bakery
+{
+    AppleStrudel, 
+    CarrotCake, 
+    ChocolateCroissant, 
+    CottageCheesePie, 
+    CottageCheeseAndCherryPie, 
+    RoseWithApplesAndCherries, 
+}
+
+public class User
+{
+    public readonly string Name;
+
+    public readonly List<Order> Orders = new();
+
+    public User(string name)
+    {
+        Name = name;
+    }
+
+    public void AddOrder(object date, object lunch, object hotFood, object soup, object bakery, object willCoffee)
+    {
+        var time = GetOrderTime((string) date);
+        var lunchReady = GetLunch((string) lunch);
+        var hotFoodReady = GetHotFood((string) hotFood);
+        var soupReady = GetSoup((string) soup);
+        var bakeryReady = GetBakery((string) bakery);
+        
+        Orders.Add(new Order(time, lunchReady, hotFoodReady, soupReady, bakeryReady, ((string) willCoffee) == "Да"));
+    }
+
+    private Bakery? GetBakery(string bakery)
+    {
+        return bakery switch
+        {
+            StringConstants.AppleStrudel => Bakery.AppleStrudel,
+            StringConstants.CarrotCake => Bakery.CarrotCake,
+            StringConstants.ChocolateCroissant => Bakery.ChocolateCroissant,
+            StringConstants.CottageCheesePie => Bakery.CottageCheesePie,
+            StringConstants.CottageCheeseAndCherryPie => Bakery.CottageCheeseAndCherryPie,
+            StringConstants.RoseWithApplesAndCherries => Bakery.RoseWithApplesAndCherries,
+            _ => null,
+        };
+    }
+
+    private Soup? GetSoup(string soup)
+    {
+        return soup switch
+        {
+            StringConstants.SpinachSoup => Soup.SpinachSoup,
+            StringConstants.MushroomSoup => Soup.MushroomSoup,
+            StringConstants.PumpkinSoup => Soup.PumpkinSoup,
+            _ => null,
+        };
+    }
+
+    private HotFood? GetHotFood(string hotFood)
+    {
+        return hotFood switch
+        {
+            StringConstants.Pork => HotFood.Pork,
+            StringConstants.Beef => HotFood.Beef,
+            StringConstants.Chicken => HotFood.Chicken,
+            StringConstants.Shrimp => HotFood.Shrimp,
+            StringConstants.FalafelBeans => HotFood.FalafelBeans,
+            StringConstants.FalafelChickpea => HotFood.FalafelChickpea,
+            StringConstants.FalafelBuckwheat => HotFood.FalafelBuckwheat,
+            StringConstants.KebabChicken => HotFood.KebabChicken,
+            StringConstants.KebabPork => HotFood.KebabPork,
+            _ => null,
+        };
+    }
+
+    private Lunch? GetLunch(string lunch)
+    {
+        return lunch switch
+        {
+            StringConstants.Manager1 => Lunch.Manager1,
+            StringConstants.Manager2 => Lunch.Manager2,
+            StringConstants.BusinessLady1 => Lunch.BusinessLady1,
+            StringConstants.BusinessLady2 => Lunch.BusinessLady2,
+            StringConstants.Freelancer1 => Lunch.Freelancer1,
+            StringConstants.Freelancer2 => Lunch.Freelancer2,
+            StringConstants.Gamer1 => Lunch.Gamer1,
+            StringConstants.Gamer2 => Lunch.Gamer2,
+            StringConstants.Vegan1 => Lunch.Vegan1,
+            StringConstants.Vegan2 => Lunch.Vegan2,
+            StringConstants.Vegan3 => Lunch.Vegan3,
+            StringConstants.Prince1 => Lunch.Prince1,
+            StringConstants.Prince2 => Lunch.Prince2,
+            _ => null
+        };
+    }
+
+    private OrderTime GetOrderTime(string date)
+    {
+        return date switch
+        {
+            StringConstants.MorningOrder => OrderTime.Morning,
+            StringConstants.DayOrder => OrderTime.Day,
+            StringConstants.NightOrder => OrderTime.Night,
+            _ => OrderTime.Default
+        };
+    }
+}
+
+public class Order
+{
+    public readonly OrderTime OrderTime;
+    public readonly Lunch? Lunch;
+    public readonly HotFood? HotFood;
+    public readonly Soup? Soup;
+    public readonly Bakery? Bakery;
+    public readonly bool WillCoffee;
+
+    public Order(OrderTime orderTime, Lunch? lunch, HotFood? hotFood, Soup? soup, Bakery? bakery, bool willCoffee)
+    {
+        OrderTime = orderTime;
+        Lunch = lunch;
+        HotFood = hotFood;
+        Soup = soup;
+        Bakery = bakery;
+        WillCoffee = willCoffee;
+    }
+}
---
+++ WorkLibary/StringConstants.cs
@@ -0,0 +1,54 @@
+public abstract class StringConstants
+{
+    // Dayly
+    public const string MorningOrder = "Утренний (9:00)";
+    public const string DayOrder = "Дневная (12:30)";
+    public const string NightOrder = "Вечерняя (15:30)";
+
+    // Lunches
+    public const string Manager1 = "ТОП-менеджер #1 (грибной суп-пюре, говядина су-вид, штрудель)";
+    public const string Manager2 = "ТОП-менеджер #2 (грибной суп-пюре, свинина, штрудель)";
+    
+    public const string BusinessLady1 = "Бизнес-леди #1 (тыквенный суп-пюре, креветка, круассан)";
+    public const string BusinessLady2 = "Бизнес-леди #2 (тыквенный суп-пюре, курица, круассан)";
+    
+    public const string Freelancer1 = "ФРИЛАНСЕР #1 (грибной суп-пюре, курица, розочка)";
+    public const string Freelancer2 = "ФРИЛАНСЕР #2 (грибной суп-пюре, кебаб (гов-свин), розочка)";
+    
+    public const string Gamer1 = "ГЕЙМЕР #1 (грибной суп-пюре, кебаб (гов-свин), пирожок с творогом и вишней)";
+    public const string Gamer2 = "ГЕЙМЕР #2 (грибной суп-пюре, кебаб (гов-кур), пирожок с творогом и вишней)";
+    
+    public const string Vegan1 = "Веган #1 (шпинатный суп-пюре, фалафель нут, морковный кекс)";
+    public const string Vegan2 = "Веган #2 (шпинатный суп-пюре, фалафель греча, морковный кекс)";
+    public const string Vegan3 = "Веган #3 (шпинатный суп-пюре, фалафель фасоль, морковный кекс)";
+
+    public const string Prince1 =
+        "СУПЕРГЕРОЙ / ПРИНЦЕССА #1 (фрикадельки, картофельный дольки с сырным соусом, пирожок с творогом)";
+    
+    public const string Prince2 =
+        "СУПЕРГЕРОЙ / ПРИНЦЕССА #2 (фрикадельки, картофельный дольки с грибным соусом, пирожок с творогом)";
+    
+    // HotFood
+    public const string Pork = "Свинина жареная на углях с овощами в лаваше";
+    public const string Beef = "Говядина су-вид с овощами в лаваше";
+    public const string Chicken = "Курица жареная на углях с овощами в лаваше";
+    public const string Shrimp = "Жареные креветки с овощами в лаваше";
+    public const string FalafelBeans = "Фалафель из фасоли с овощами в лаваше";
+    public const string FalafelChickpea = "Фалафель из нута с овощами в лаваше";
+    public const string FalafelBuckwheat = "Фалафель из гречи с овощами в лаваше";
+    public const string KebabChicken = "Люля-кебаб говядина курица с овощами в лаваше";
+    public const string KebabPork = "Люля-кебаб говядина свинина с овощами в лаваше";
+    
+    // Soup
+    public const string SpinachSoup = "Суп-пюре шпинатный";
+    public const string MushroomSoup = "Суп-пюре грибной";
+    public const string PumpkinSoup = "Суп-пюре тыквенный";
+    
+    // Bakery
+    public const string AppleStrudel = "Слойка \"Яблочный штрудель\"";
+    public const string CarrotCake = "Кекс морковный";
+    public const string ChocolateCroissant = "Круассан с шоколадом";
+    public const string CottageCheesePie = "Пирожок с творогом";
+    public const string CottageCheeseAndCherryPie = "Пирожок с творогом и вишней";
+    public const string RoseWithApplesAndCherries = "Розочка с яблоками и вишней";
+}
